0b05899201d649f26671db3f34878098d00fd7eb
LucioBehr <lucio.behr@outlook.com>
2023-05-31 20:04:13 -0300

    commit 03 310523- base do servidor e conexao estabelecida

--- client/Client.java
+++ client/Client.java
@@ -1,5 +1,34 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
 public class Client {
-    
+    public static void main(String[] args) {
+        try {
+            //start do client
+            Socket socket = new Socket("localhost", 3333);
+
+            //declaracao de in e out do client
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintStream out = new PrintStream(socket.getOutputStream(), true);
+
+            //declaracao do envio de mensagem pelo client
+            String message = in.readLine();
+            System.out.println(message);
+
+            //leitura do que o usuario digitar.
+            BufferedReader consoleIn = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+            }
+            //in.close();
+            //out.close();
+            //socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- server/Server.java
+++ server/Server.java
@@ -1,5 +1,33 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-    
+    public static void main(String[] args) {
+        try {
+            // start do servidor
+            ServerSocket serverSocket = new ServerSocket(3333);
+            System.out.println("Aguardando conexão dos players.");
+
+            //declaracao da conexao do player
+            Socket player = serverSocket.accept();
+
+
+            //declaracao do in e out do servidor
+            BufferedReader in = new BufferedReader(new InputStreamReader(player.getInputStream()));
+            PrintStream out = new PrintStream(player.getOutputStream(), true);
+
+
+            //Conexão estabelecida com jogador
+            System.out.println("Jogador 1 entrou. Aguardando novo player.");
+            out.println("Bem-vindo, jogador 1. Aguarde a conexão de um novo player.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
